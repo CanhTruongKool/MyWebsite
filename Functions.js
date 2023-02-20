5ba210a81430bc41ed7e095a47fe8bfc0cc0141a
Canh Truong <canhtruong2412@gmail.com>
2023-02-20 23:34:08 +0700

    20/2/2023

---
+++ Functions.js
@@ -0,0 +1,11 @@
+alert("Hello, I wish you have a nice day !!!")
+function Popup()
+{
+    var d = document.getElementById("PopupContent");
+    d.style.display =  "block";
+}
+function Popout()
+{
+    var d = document.getElementById("PopupContent");
+    d.style.display =  "none";
+}
