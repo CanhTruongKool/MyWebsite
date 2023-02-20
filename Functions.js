816178a3097f1f26d7d642114d9e82eb08ff54e0
Canh Truong <canhtruong2412@gmail.com>
2023-02-21 01:40:30 +0700

    early morning 21/2/2023

--- Functions.js
+++ Functions.js
@@ -1,4 +1,14 @@
 alert("Hello, I wish you have a nice day !!!")
+function ContactDropDown()
+{
+    var d = document.getElementById("Contact");
+    d.href = "https://www.youtube.com/"
+}
+function ContactDropNone()
+{
+    var d = document.getElementById("Contact");
+    d.href =""
+}
 function Popup()
 {
     var d = document.getElementById("PopupContent");
