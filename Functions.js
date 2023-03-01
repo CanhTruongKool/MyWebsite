bdf4f0385d2bb404198b08ff43dc83f21a0c2a99
Canh Truong <canhtruong2412@gmail.com>
2023-03-01 23:16:28 +0700

    make nav for mobile version

--- Functions.js
+++ Functions.js
@@ -19,3 +19,14 @@ function Popout()
     var d = document.getElementById("PopupContent");
     d.style.display =  "none";
 }
+function menuOut(){
+   var menu = document.getElementById("header-menu-drop")
+   if(menu.style.display == "" || menu.style.display == "none") 
+   {
+    menu.style.display = "block"
+    }
+    else if(menu.style.display == "block")
+    {
+        menu.style.display = "none"
+    }
+}
